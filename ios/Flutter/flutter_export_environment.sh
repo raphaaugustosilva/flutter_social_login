1d46cdd07641a56de5f7281f60a856e0ad63f4ce
Raphael Augusto da Silva <raphaaugustosilva@outlook.com>
2019-09-23 21:10:52 -0300

    Iniciando implementação de Social Login

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/raphael.silva/Desktop/flutter_social_login"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=/Users/raphaelaugustosilva/Programas/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/raphaelaugustosilva/Projetos/Estudo/flutter_social_login"
+export "FLUTTER_TARGET=/Users/raphaelaugustosilva/Projetos/Estudo/flutter_social_login/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Developer/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/raphaelaugustosilva/Programas/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
